6a95164cb1edc2da1538b95aac1696a269985d02
Alysson Santos <salysson280@gmail.com>
2023-11-08 14:51:01 -0300

    now

---
+++ app/Http/Controllers/PrateleiraController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrateleiraController extends Controller
+{
+    public function getAll()
+    {
+        return response()->json([]);
+    }
+
+    public function create ()
+    {
+        return response()->json([]);
+    }
+
+    public function update ()
+    {
+        return response()->json([]);
+    }
+
+    public function edit ()
+    {
+        return response()->json([]);
+    }
+
+    public function destroy ()
+    {
+        return response()->json([]);
+    }
+
+
+}
