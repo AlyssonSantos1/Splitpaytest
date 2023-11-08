d51dc2e6431cad78b506fed48dbd1246d986f2e5
Alysson Santos <salysson280@gmail.com>
2023-11-08 20:56:55 -0300

    atual

---
+++ resources/views/updateproduto.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <label for="">Nome do Produto</label>
+        <input type="text" placeholder="Nome do Produto" name="nome_item">
+        <br><br>
+        <label for="">Descricao do Produto</label>
+        <input type="text" placeholder="Descricao do Produto" name="descricao_item">
+        <br><br>
+        <label for="">Tipo do produto</label>
+        <input type="text" placeholder="Tipo do produto" name="tipo_item">
+        <br><br>
+        <label for="">Quantidade em Estoque</label>
+        <input type="text" placeholder="Quantidade em Estoque" name="preco_item">
+        <br><br>
+        <button>Enviar</button>
+
+</body>
+</html>
